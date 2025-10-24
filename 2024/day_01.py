a5c932b9579a2c45e96de1b7b00ef8832a539805
Julian Jung <tick.73spatial@icloud.com>
2025-10-24 15:04:21 +0200

    using real data

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -4,10 +4,20 @@ https://adventofcode.com/2024/day/1
 import random
 
 # Generate two random lists that we want to compare
-l1 = [random.randint(0,9) for i in range(6)]
-l2 = [random.randint(0,9) for i in range(6)]
+# l1 = [random.randint(0,9) for i in range(6)]
+# l2 = [random.randint(0,9) for i in range(6)]
 
-print(l1, l2)
+# Using input Lists
+with open("input01.txt") as f:
+    data = f.read().strip().splitlines()
+
+l1 = []
+l2 = []
+
+for line in data:
+    a, b = map(int, line.split())
+    l1.append(a)
+    l2.append(b)
 
 # Sort the lists
 l1.sort()
@@ -20,5 +30,4 @@ for entry in l1:
     dist = abs(l1[i] - l2[i])
     sum += dist
     i += 1
-
 print(sum)

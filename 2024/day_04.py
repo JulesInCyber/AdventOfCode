97f462e1098c16c27c2ce6264ef6642b33720e41
Julian Jung <tick.73spatial@icloud.com>
2025-10-28 15:05:16 +0100

    first part diagonal

--- 2024/day_04.py
+++ 2024/day_04.py
@@ -26,4 +26,25 @@ def col_search(grid: list[list[str]], pattern: str) -> int:
                 count += 1
     return count
 
+def dia_search(grid, pattern):
+    count = 0
+    max_rows = len(grid)
+    max_cols = len(grid[0])
+    pattern_length = len(pattern)
+
+    for row in range(max_rows - pattern_length + 1):
+        for col in range(max_cols - pattern_length + 1):
+            word = ''.join(grid[row+i][col+i] for i in range(pattern_length))
+            if word == pattern:
+                count += 1
+    return count
 pat = "XMAS"
+
+
+row_match = row_search(data, pat) + row_search(data, pat[::-1])
+col_match = col_search(nested, pat) + col_search(nested, pat[::-1])
+dia_match = dia_search(nested, pat) + dia_search(nested, pat[::-1])
+
+total = row_match + col_match + dia_match
+
+print(total)

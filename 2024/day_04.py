90c9ba6776a093647748d38738b5b7af677e01f3
Julian Jung <tick.73spatial@icloud.com>
2025-10-28 10:16:04 +0100

    Searching in cols (forward

--- 2024/day_04.py
+++ 2024/day_04.py
@@ -27,9 +27,26 @@ def backward_row(grid: list[str], pattern: str) -> int:
             count  += 1
     return count
 
-def forward_col(grid: list[list[str]]):
-    pass
+def forward_col(grid: list[list[str]], pattern: str) -> int:
+    count = 0
+    max_rows = len(grid)
+    max_cols = len(grid[0])
+    pattern = list(pattern) # Each Letter as list-entry
+    for row in range(max_rows):
+        for col in range(max_cols):
+            if grid[row][col] == 'X':
+                if row + 1 < max_cols:
+                    if grid[row+1][col] == 'M':
+                        if row + 2 < max_cols:
+                            if grid[row+2][col] == 'A':
+                                if row + 3 < max_cols:
+                                    if grid[row+3][col] == 'S':
+                                        count += 1
+    return count
+
 
 fr = forward_row(data, "XMAS")
 br = backward_row(data, "SAMX")
+fc = forward_col(nested, "XMAS")
+print(fc)
 print(fr + br)

065595eae8d5db8b46f1141f7a198ce3c926dedb
Julian <tick.73spatial@icloud.com>
2025-12-02 12:21:58 +0100

    working on new method

--- 2025/001.py
+++ 2025/001.py
@@ -1,7 +1,7 @@
 import re
 
 # Test Data
-with open("D:/GitHub/AdventOfCode/2025/001_live.txt") as file:
+with open("D:/GitHub/AdventOfCode/2025/001_test.txt") as file:
     data = file.read().strip().splitlines()
 
 def get_algorithm(data):
@@ -29,8 +29,32 @@ def turn_dial(dial_start, instructions):
     code = f"The Code is: {zeroes}"
     return code  
 
+def method_0x434C49434B(dial_start, instructions):
+    '''
+    - Number of times dial hits 0 --> Done
+    - Number of times dial crosses 0
+    '''
+    clicks = 0
+    for i in instructions:
+        old_dial = dial_start
+        #print(f"Older: {dial_start}")
+        if i[0] == "L":
+            result = dial_start - int(i[1])
+            # print(f"L Result is: {result}")
+            dial_start = result % 100
+        elif i[0] =="R":
+            result = dial_start + int(i[1])
+            # print(f"R Result is: {result}")
+            dial_start = result % 100
+        if dial_start == 0:
+            clicks += 1
+
+    code = f"The Code is: {clicks}"
+    return code
+
+
 instructions = get_algorithm(data)
-code = turn_dial(50, instructions)
+code = method_0x434C49434B(50, instructions)
 
 print(code)
 

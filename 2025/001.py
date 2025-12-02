1be9726314828ff2687108e1e159cbe1fd1bfdc1
Julian <tick.73spatial@icloud.com>
2025-12-02 11:41:19 +0100

    Working for test data

--- 2025/001.py
+++ 2025/001.py
@@ -4,12 +4,33 @@ import re
 with open("D:/GitHub/AdventOfCode/2025/001_test.txt") as file:
     data = file.read().strip().splitlines()
 
-only_nums = []
-dial = 50
-zeroes = 0
+def get_algorithm(data):
+    algorithm = []
+    for entry in data:
+        pattern = entry.split()
+        for item in pattern:
+            alg = re.split(r'(L|R)', item)
+            filtered = [i for i in alg if i != ""]
+            algorithm.append(filtered)
+    return algorithm
 
-for entry in data:
-    pattern = entry.split()
-    for item in pattern:
-        alg = re.split(r'(L|R)', item)
-        print(alg)
+def turn_dial(dial_start, instructions):
+    zeroes = 0
+    for i in instructions:
+        print(f"Dial at: {dial_start}")
+        if dial_start == 0:
+            zeroes += 1
+        if i[0] == "L":
+            result = dial_start - int(i[1])
+            dial_start = result % 100
+        elif i[0] =="R":
+            result = dial_start + int(i[1])
+            dial_start = result % 100
+    code = f"The Code is: {zeroes}"
+    return code  
+
+instructions = get_algorithm(data)
+code = turn_dial(50, instructions)
+
+print(code)
+

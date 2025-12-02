b442d0a8bda89a31ccc1db7e963f65ba83ad3bec
JulesInCyber <tick.73spatial@icloud.com>
2025-12-02 21:01:27 +0100

    Used Methods to find sequence

--- 2025/002.py
+++ 2025/002.py
@@ -16,28 +16,31 @@ def double_sequence(start, end, seq:list=[]):
         if re.search(pattern, str(number)):
             seq.append(number)
     return seq
+
+def any_sequence(start, end, seq:list):
+    pattern = r"^(\d+)\1+$"
+    for number in range(start, end+1):
+        if re.search(pattern,str(number)):
+            seq.append(number)
+    return seq
+        
+
 # Preparing The Data
-with open("./002_test.txt") as file:
+with open("./002_live.txt") as file:
     data = file.read().split(',')
-
-# sum = 0
-# # Iteration through ranges
-# for numbers in data:
-#     range_start = int(numbers.split('-')[0])
-#     range_end = int(numbers.split('-')[1])+1
-#     pattern = r"^(\d+)\1+$" # delete the last '+' for exactly 2
-#     for num in range(range_start,range_end):
-#         if re.search(pattern, str(num)):
-#             sum += num
-
-# print(sum)
-
+    
 x = []
 for r in data:
     range_start = int(r.split('-')[0])
     range_end = int(r.split('-')[1])
     double_sequence(range_start,range_end,x)
 
+y = []
+for r in data:
+    range_start = int(r.split('-')[0])
+    range_end= int(r.split('-')[1])
+    any_sequence(range_start, range_end, y)
 # double_sequence(11,22, x)
 # double_sequence(998,1012,x)
 print(sum(x))
+print(sum(y))

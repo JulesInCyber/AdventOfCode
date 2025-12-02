12b1156d9dfc882725d875c118cf7bb34387d6b5
Julian <tick.73spatial@icloud.com>
2025-12-02 11:00:15 +0100

    Separated Direction and Number

--- 2025/001.py
+++ 2025/001.py
@@ -1,5 +1,15 @@
+import re
+
 # Test Data
 with open("D:/GitHub/AdventOfCode/2025/001_test.txt") as file:
     data = file.read().strip().splitlines()
 
-print(data)
+only_nums = []
+dial = 50
+zeroes = 0
+
+for entry in data:
+    pattern = entry.split()
+    for item in pattern:
+        alg = re.split(r'(L|R)', item)
+        print(alg)

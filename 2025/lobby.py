e9f456666f4c72fd3485ed2b197ad6c96de98cb7
Julian <tick.73spatial@icloud.com>
2025-12-04 13:26:32 +0100

    Technically Solved but Memory Error

--- 2025/lobby.py
+++ 2025/lobby.py
@@ -6,22 +6,33 @@ Day: 03/12
 
 import re
 import itertools
-MAXIMUM_JOLTAGE = 0
 
-# Opening Data
-with open("./003_live.txt") as file:
-    data = file.read().strip().splitlines()
+def Maximum_Joltage_1(filepath):
+    MAXIMUM_JOLTAGE = 0
+    with open(filepath) as file:
+        data = file.read().strip().splitlines()
+    for battery in data:
+        jolts = []
+        digits = [d for d in battery]
+        # print(digits)
+        combo = list(itertools.combinations(digits, 2))
+        for c in combo:
+            jolts.append("".join(c))
+        MAXIMUM_JOLTAGE += int(max(jolts))
+    return MAXIMUM_JOLTAGE
 
-# print(data)
-for battery in data:
-    jolts = []
-    digits = [d for d in battery]
-    # print(digits)
-    combo = list(itertools.combinations(digits, 2))
-    for c in combo:
-        jolts.append("".join(c))
-    MAXIMUM_JOLTAGE += int(max(jolts))
+def Maximum_Joltage_2(filepath):
+    output = 0
+    with open(filepath) as file:
+        data = file.read().strip().splitlines()
+    for battery in data:
+        jolts = []
+        digits = [d for d in battery]
+        combo = list(itertools.combinations(digits,12))
+        for c in combo:
+            jolts.append("".join(c))
 
-print(MAXIMUM_JOLTAGE)
+        output += int(max(jolts))
+    print(output)
 
-# Maximum_Joltage(["987654321111111"])
+Maximum_Joltage_2("./003_live.txt")
